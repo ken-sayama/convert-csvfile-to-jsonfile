53c273b13f900c4bb814e07a181741279760122c
Ken Sayama <sayamaken0402@gmail.com>
2019-10-26 20:46:05 +0900

    fux

---
+++ index.rb
@@ -0,0 +1,16 @@
+require 'json'
+require 'csv'
+
+class ConvertCsvfileToJsonfile
+  def initialize
+    # csvファイル名を設定
+    csvfilename = ''
+    # jsonファイル名を設定
+    jsonfilename = ''
+
+    body = File.open(filename).read
+    csv = CSV.new(body, headers: true, header_converters: :symbol, force_quotes: true)
+    rows = csv.to_a.map { |row| row.to_hash }
+    File.open(jsonfilename, "w") { |f| f.write rows.to_json }
+  end
+end
