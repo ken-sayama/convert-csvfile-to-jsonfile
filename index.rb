053fb3d27f9cf2cee65d2c41c92e7ddc864b6fcf
Ken Sayama <sayamaken0402@gmail.com>
2019-10-26 20:50:33 +0900

    change dir path

--- index.rb
+++ index.rb
@@ -8,9 +8,9 @@ class ConvertCsvfileToJsonfile
     # jsonファイル名を設定
     jsonfilename = ''
 
-    body = File.open(filename).read
+    body = File.open("./csv/#{filename}").read
     csv = CSV.new(body, headers: true, header_converters: :symbol, force_quotes: true)
     rows = csv.to_a.map { |row| row.to_hash }
-    File.open(jsonfilename, "w") { |f| f.write rows.to_json }
+    File.open("./json/#{jsonfilename}", "w") { |f| f.write rows.to_json }
   end
 end
